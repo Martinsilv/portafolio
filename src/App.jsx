0c0835482d4944f275af61c6726a3146061a0025
Martin Silva <martinsilva510@gmail.com>
2025-10-13 02:11:50 -0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,10 @@ import "./App.css";
 
 import { Inicio } from "./components/inicio.jsx";
 import { Proyects } from "./components/proyects.jsx";
-import { Toaster } from "react-hot-toast";
+
 function App() {
   return (
     <>
-      <Toaster
-        toastOptions={{
-          style: {
-            background: "#18181b",
-            color: "#fff",
-            border: "1px solid #e3e3e3",
-          },
-        }}
-      />
       <Inicio />
 
       <Proyects />
